76db21ee33353bb692a25ed8ae73214c143e2d05
Arthur Sven Msiska <arthursvenmsiska@gmail.com>
2023-04-18 16:55:45 +0200

    Created Agent, Home and Team fragment, need to create the logic to switch fragments

--- app/src/main/java/com/example/mwyouthfootballportal/UI/Fragments/TeamFragment.kt
+++ app/src/main/java/com/example/mwyouthfootballportal/UI/Fragments/TeamFragment.kt
@@ -8,14 +8,15 @@ import android.view.ViewGroup
 import com.example.mwyouthfootballportal.R
 
 
-class HomeFragment : Fragment() {
+class TeamFragment : Fragment() {
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        return inflater.inflate(R.layout.fragment_team, container, false)
     }
 
 }
